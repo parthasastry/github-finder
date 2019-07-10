604fa34457a94b9cb800ff2354527a4b61dbf4f3
Sastry <110067@cognizant.com>
2019-07-10 12:19:30 -0400

    React hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -10,15 +10,13 @@ import axios from 'axios';
 import './App.css';
 
 
-class App extends Component {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
 
-  state = {
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-    alert: null
-  }
 
   // async componentDidMount() {
   //   
@@ -28,63 +26,67 @@ class App extends Component {
   //   this.setState({users: res.data, loading: false})
   // }
 
-  searchUsers = async text => {
-    this.setState({ loading: true });
+  const searchUsers = async text => {
+    setLoading(true);
 
     const url = `https://api.github.com/search/users?q=${text}`;
 
     const res = await axios.get(url)
 
-    this.setState({ users: res.data.items, loading: false })
+    setUsers(res.data.items);
+    setLoading(false)
   }
 
   //get a single github user data
-  getUser = async (username) =>  {
-    this.setState({ loading: true });
+  const getUser = async (username) =>  {
+    setLoading(true);
 
     const url = `https://api.github.com/users/${username}`;
 
     const res = await axios.get(url)
 
-    this.setState({ user: res.data, loading: false })
+    setUser(res.data);
+    setLoading(false);
   }
 
   //Get users repos
-  getUserRepos = async (username) =>  {
-    this.setState({ loading: true });
+  const getUserRepos = async (username) =>  {
+    setLoading(true);
 
     const url = `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`;
 
     const res = await axios.get(url)
 
-    this.setState({ repos: res.data, loading: false })
+    setRepos(res.data);
+    setLoading(false);
   }
 
   //Clear users
-  clearUsers = () => this.setState({ users: [], loading: false })
-
-  setAlert = (msg, type) => {
-    this.setState({ alert: { msg: msg, type: type } })
-
-    setTimeout(() => this.setState({ alert: null }), 5000)
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
   }
 
-  render() {
-    const { users, user, loading, repos } = this.state;
+  const showAlert = (msg, type) => {
+    setAlert({ msg: msg, type: type })
+
+    setTimeout(() => setAlert(null), 5000);
+  }
+
     return (
       <Router>
         <div className='App'>
           <Navbar />
           <div className="container">
-            <Alert alert={this.state.alert} />
+            <Alert alert={alert} />
             <Switch>
               <Route exact path="/" render={props => (
                 <Fragment>
                   <Search
-                    searchUsers={this.searchUsers}
-                    clearUsers={this.clearUsers}
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
                     showClear={users.length > 0 ? true : false}
-                    setAlert={this.setAlert}
+                    setAlert={showAlert}
                   />
                   <Users loading={loading} users={users} />
                 </Fragment>
@@ -93,8 +95,8 @@ class App extends Component {
               <Route exact path='/user/:login' render={props => (
                 <User 
                   {...props} 
-                  getUser={this.getUser} 
-                  getUserRepos={this.getUserRepos} 
+                  getUser={getUser} 
+                  getUserRepos={getUserRepos} 
                   user={user} loading={loading} 
                   repos={repos}
                 />
@@ -106,7 +108,6 @@ class App extends Component {
       </Router>
 
     );
-  }
 }
 
 export default App;
